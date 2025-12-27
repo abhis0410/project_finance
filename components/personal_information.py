f8a4843e1313b2bbcb0f3e3f02947d12aa24f5ad
abhis0410 <abhinavs0410@gmail.com>
2025-12-27 16:19:13 +0530

    fix personal_information.py

--- components/personal_information.py
+++ components/personal_information.py
@@ -4,15 +4,16 @@ from utils.field import Field, FieldType
 from utils.form import Form
 
 
-def personal_information_form():
+def personal_information_form(key_prefix: str):
     fields = [
-        Field("first_name", "First name", FieldType.STRING),
-        Field("last_name", "Last name", FieldType.STRING),
+        Field("first_name", "First name", FieldType.STRING, key_prefix),
+        Field("last_name", "Last name", FieldType.STRING, key_prefix),
 
         Field(
             "sin",
             "Social Insurance Number",
             FieldType.STRING,
+            key_prefix,
             help_text="9-digit number",
         ),
 
@@ -20,6 +21,7 @@ def personal_information_form():
             "date_of_birth",
             "Date of birth",
             FieldType.DATE,
+            key_prefix,
             default=date(1990, 1, 1),
         ),
 
@@ -27,42 +29,24 @@ def personal_information_form():
             "province_2025",
             "Province of residence on December 31, 2025",
             FieldType.STRING,
+            key_prefix,
         ),
 
         Field(
             "marital_status_2025",
             "Marital status on December 31, 2025",
             FieldType.SELECT,
+            key_prefix,
             options=["Single", "Married", "Common-law", "Divorced", "Widowed"],
         ),
 
-        Field("street", "Street address", FieldType.STRING),
-        Field("city", "City", FieldType.STRING),
-        Field("address_province", "Province", FieldType.STRING),
-        Field("postal_code", "Postal code", FieldType.STRING),
+        Field("street", "Street address", FieldType.STRING, key_prefix),
+        Field("city", "City", FieldType.STRING, key_prefix),
+        Field("address_province", "Province", FieldType.STRING, key_prefix),
+        Field("postal_code", "Postal code", FieldType.STRING, key_prefix),
 
-        Field("home_phone", "Home phone number", FieldType.STRING, required=False),
-        Field("email", "Email address", FieldType.STRING),
+        Field("home_phone", "Home phone number", FieldType.STRING, key_prefix, required=False),
+        Field("email", "Email address", FieldType.STRING, key_prefix),
     ]
 
-    form = Form(
-        form_id="personal_information",
-        title="Personal Information",
-        fields=fields,
-    )
-
-    return form.render()
-
-
-def main():
-    st.title("Tax Information Form")
-    data, errors = personal_information_form()
-
-    if st.button("Submit", key="submit_personal_info"):
-        if errors:
-            st.error("Please fix the highlighted errors.")
-            st.json(errors)
-        else:
-            return data
-
-    return None
+    return fields
